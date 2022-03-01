fe85314650f11a9269f27612a3da11b2b034de3f
pazi09 <youngsaintforwait@gmail.com>
2022-03-01 18:25:10 +0400

    City connection established

--- src/main/java/com/example/CommunityAppMessanger/serviceInterface/CityServiceInterface.java
+++ src/main/java/com/example/CommunityAppMessanger/serviceInterface/CityServiceInterface.java
@@ -1,4 +1,20 @@
 package com.example.CommunityAppMessanger.serviceInterface;
 
+import com.example.CommunityAppMessanger.models.Cities;
+import com.example.CommunityAppMessanger.models.Houses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public interface CityServiceInterface {
+    ResponseEntity<Cities> saveCity(Cities city);
+
+    ResponseEntity<List<Cities>> findAll();
+
+    ResponseEntity<Cities> findById (Long Id);
+
+    ResponseEntity<HttpStatus> deleteById(Long id);
+
+    ResponseEntity<Cities> updateCity(Long id, Cities city);
 }
