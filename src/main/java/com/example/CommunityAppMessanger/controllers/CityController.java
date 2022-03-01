7c1c746f4b23e9c8a07674666e1196008016de69
pazi09 <youngsaintforwait@gmail.com>
2022-03-01 18:25:34 +0400

    City connection established

---
+++ src/main/java/com/example/CommunityAppMessanger/controllers/CityController.java
@@ -0,0 +1,51 @@
+package com.example.CommunityAppMessanger.controllers;
+
+
+import com.example.CommunityAppMessanger.models.Cities;
+import com.example.CommunityAppMessanger.models.Houses;
+import com.example.CommunityAppMessanger.serviceInterface.CityServiceInterface;
+import com.example.CommunityAppMessanger.serviceInterface.HouseServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/city")
+public class CityController {
+
+    private final CityServiceInterface cityService;
+
+    @Autowired
+    public CityController(CityServiceInterface cityService){
+        this.cityService=cityService;
+    }
+
+    @GetMapping(path = "")
+    public ResponseEntity<List<Cities>> getCity() {
+        return cityService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cities> getCity(@PathVariable("id") Long id) {
+        return cityService.findById(id);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Cities> addCity(@RequestBody Cities cities){
+        return cityService.saveCity(cities);
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Cities> updateCity(@PathVariable(value = "id") Long id, @RequestBody Cities cities) {
+        return cityService.updateCity(id, cities);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteCity(@PathVariable(value = "id") Long id) {
+        return cityService.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/CommunityAppMessanger/services/CityService.java
@@ -0,0 +1,86 @@
+package com.example.CommunityAppMessanger.services;
+
+
+import com.example.CommunityAppMessanger.models.Cities;
+import com.example.CommunityAppMessanger.models.Flats;
+import com.example.CommunityAppMessanger.repository.CityRepository;
+import com.example.CommunityAppMessanger.serviceInterface.CityServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityService implements CityServiceInterface {
+
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public  CityService(CityRepository cityRepository){
+        this.cityRepository=cityRepository;
+    }
+
+    @Override
+    public ResponseEntity<Cities> saveCity(Cities city) {
+        try {
+            Cities newCity=cityRepository.save(new Cities(city.getCity()));
+            return new ResponseEntity<>(newCity, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<List<Cities>> findAll() {
+        try {
+            List<Cities> cities = new ArrayList<Cities>();
+            cityRepository.findAll().forEach(cities::add);
+            if (cities.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(cities, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Cities> findById (Long Id) {
+        Optional<Cities> cities = cityRepository.findById(Id);
+
+        if (cities.isPresent()) {
+            return new ResponseEntity<>(cities.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public ResponseEntity<HttpStatus> deleteById(Long id) {
+        try {
+            cityRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Cities> updateCity(Long id, Cities cities) {
+        Optional<Cities> cityDB = cityRepository.findById(id);
+
+        if (cityDB.isPresent()) {
+            Cities _cities = cityDB.get();
+            _cities.setCity(cities.getCity());
+
+            return new ResponseEntity<>(cityRepository.save(_cities), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
